cc2af4ec18cbdeca793d5b6d07efe26d35b45a59
Andrea Pani <andre@Andreas-MacBook-Pro.local>
2022-03-23 08:45:53 +0100

    fix nfts endpoint

--- src/features/misc/routes/NftDetail.tsx
+++ src/features/misc/routes/NftDetail.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/componentes/Elements/Button/Button';
 import { MainLayout } from '@/componentes/Layout/MainLayout';
-import { Nft } from '@/lib/api/nfts';
+import { Nft, NFTListed } from '@/lib/api/nfts';
+import { httpClient } from '@/lib/httpClient';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -8,20 +9,20 @@ import algoLogo from '../../../assets/algoLogo.svg';
 
 export const NftDetail = () => {
   const { ipnft } = useParams();
-  const [data, setData] = useState<any[]>();
+  const [error, setError] = useState<any>();
+  const [data, setData] = useState<NFTListed[]>();
 
   useEffect(() => {
-    mockFetch();
+    fetchNfts();
   }, [ipnft]);
 
-  const mockFetch = () => {
-    axios
-      .get(`http://127.0.0.1:8080/api/v1/nfts.json/`)
-      .then((res) => {
-        setData(res.data);
-        console.log('res.data', res.data);
-      })
-      .catch((err) => console.log(err));
+  const fetchNfts = async () => {
+    try {
+      const res = await httpClient.get('nfts');
+      setData(res.data);
+    } catch (err: any) {
+      setError(err.message);
+    }
   };
 
   const nftSelected = data?.find((nft) => nft.ipnft === ipnft);
@@ -59,6 +60,7 @@ export const NftDetail = () => {
           </Button>
         </div>
       </div>
+      {error && <div>{error}</div>}
     </MainLayout>
   );
 };
