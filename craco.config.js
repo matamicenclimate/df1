b4405d87f58a9311b4c88b56dbdb92bf6710e29e
pblanco-dekalabs <pblanco@dekalabs.com>
2022-03-24 12:06:47 +0100

    Added teal code generator

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,7 @@
+// const { writeFileSync } = require('fs');
+// const { format } = require('prettier');
 const path = require('path');
+const cp = require('child_process');
 
 module.exports = {
   webpack: {
@@ -6,11 +9,25 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
     configure: (conf) => {
+      console.log('Compiling smart contract IR...');
+      cp.execSync(`python auction/contracts.py`, {
+        cwd: 'contracts',
+      });
+      console.log('Done! Output to src/lib/contracts');
       conf.resolve.fallback = {
         crypto: require.resolve('crypto-browserify'),
         stream: require.resolve('stream-browserify'),
         buffer: require.resolve('buffer'),
       };
+      conf.module.rules[1].oneOf.unshift({
+        test: /\.teal$/i,
+        use: [
+          {
+            loader: require.resolve('raw-loader'),
+          },
+        ],
+      });
+      // writeFileSync('config.js', format(`module.exports = ${JSON.stringify(conf)}`));
       return conf;
     },
   },
---
+++ src/raw-loader.d.ts
@@ -0,0 +1,4 @@
+declare module '*.teal' {
+  const content: string;
+  export default content;
+}
