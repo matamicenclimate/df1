f6981026b95113cac5c09accab3df9a3103a38ed
pblanco-dekalabs <pblanco@dekalabs.com>
2022-04-12 12:53:05 +0200

    NFT cleanup

--- src/lib/nft.ts
+++ src/lib/nft.ts
@@ -14,64 +14,35 @@ export interface AssetInfo {
   assetID: number;
 }
 
-async function createAsset(
+async function createAsset<A extends Record<string, any> = any>(
   algodClient: algosdk.Algodv2,
   account: string,
-  metadat: any,
-  wallet: any
+  meta: A,
+  wallet: Wallet
 ) {
   dialog.message = 'Checking blockchain connection...';
   //Check algorand node status
   const status = await algodClient.status().do();
-  console.log('statusstatusstatusstatus', status);
   //Check account balance
   dialog.message = 'Retrieving account information...';
   const accountInfo = await algodClient.accountInformation(account).do();
-  console.log('accountInfo', accountInfo);
-
   const startingAmount = accountInfo.amount;
   console.log('User account balance: microAlgos', startingAmount);
-
   // Construct the transaction
   const params = await algodClient.getTransactionParams().do();
-  console.log('params', params);
-  // comment out the next two lines to use suggested fee
-  // params.fee = 1000;
-  // params.flatFee = false;
-  // const closeout = receiver; //closeRemainderTo
-  // WARNING! all remaining funds in the sender account above will be sent to the closeRemainderTo Account
-  // In order to keep all remaining funds in the sender account after tx, set closeout parameter to undefined.
-  // For more info see:
-  // https://developer.algorand.org/docs/reference/transactions/#payment-transaction
-  // Asset creation specific parameters
-  // The following parameters are asset specific
-  // Throughout the example these will be re-used.
-
   const defaultFrozen = false;
   // Friendly name of the asset
-  const assetName = metadat.title;
+  const assetName = meta.title;
   // Optional string pointing to a URL relating to the asset
-  const url = metadat.url;
-  // const managerAddr = 'ILHMK67WCDOW3RHYZWTYPOVSXJOIETARELRLATUSVXV2TTTYIN47X4GE3E'; // OPTIONAL: FOR DEMO ONLY, USED TO DESTROY ASSET WITHIN
+  const url = meta.url;
   const managerAddr = account;
   const reserveAddr = account;
   const freezeAddr = account;
   const clawbackAddr = account;
-
-  // Use actual total  > 1 to create a Fungible Token
-  // example 1:(fungible Tokens)
-  // totalIssuance = 10, decimals = 0, result is 10 total actual
-  // example 2: (fractional NFT, each is 0.1)
-  // totalIssuance = 10, decimals = 1, result is 1.0 total actual
-  // example 3: (NFT)
-  // totalIssuance = 1, decimals = 0, result is 1 total actual
   // integer number of decimals for asset unit calculation
   const decimals = 0;
   const total = 1; // how many of this asset there will be
-
-  const metadataHash = mdhash.digest(metadat);
-  console.log('metadataHash', metadataHash);
-
+  const metadataHash = mdhash.digest(meta);
   dialog.message = 'Sending NFT data...';
   const txn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
     from: account,
@@ -80,7 +51,7 @@ async function createAsset(
     assetName: assetName,
     assetURL: url,
     assetMetadataHash: metadataHash,
-    note: algosdk.encodeObj(metadat),
+    note: algosdk.encodeObj(meta),
     defaultFrozen,
     freeze: freezeAddr,
     manager: managerAddr,
@@ -88,19 +59,7 @@ async function createAsset(
     reserve: reserveAddr,
     suggestedParams: params,
   });
-  console.log('txn', txn);
-
-  // const rawSignedTxn = txn.signTxn(recoveredAccount2.sk);
-  // const rawSignedTxn = await wallet.signTxn([txn]);
-  // const ctx = await algodClient.sendRawTransaction(rawSignedTxn).do();
-  // // Wait for confirmation
-  // const confirmedTxn = await algosdk.waitForConfirmation(algodClient, ctx.txId, 4);
-  // //Get the completed Transaction
-  // console.log('Transaction ' + ctx.txId + ' confirmed in round ' + confirmedTxn['confirmed-round']);
-
   const [s_create_txn] = await wallet.signTxn([txn]);
-  console.log('[s_create_txn]', [s_create_txn]);
-
   dialog.message = 'Sending NFT data...';
   const { txId } = await algodClient
     .sendRawTransaction(
@@ -109,31 +68,27 @@ async function createAsset(
       })
     )
     .do();
-
   dialog.message = 'Waiting for confirmation...';
   const confirmedTxn = await algosdk.waitForConfirmation(algodClient, txId, 10);
   console.log('Transaction ' + txId + ' confirmed in round ' + confirmedTxn['confirmed-round']);
   const assetID = confirmedTxn['asset-index'];
   console.log('AssetID = ' + assetID);
-
   await printCreatedAsset(algodClient, account, assetID);
   await printAssetHolding(algodClient, account, assetID);
-
   const assetInfo: AssetInfo = {
     transactionId: txId,
     assetID: assetID,
   };
-
   return assetInfo;
 }
 
-export async function destroyAsset(algodClient: any, account: any, assetID: any, wallet: any) {
-  console.log('account', account);
-  console.log('algodClient', algodClient);
-  console.log('assetID', assetID);
-  console.log('assetID', typeof assetID);
-  console.log('==> DESTROY ASSET');
-  const assetNumber = Number(assetID);
+export async function destroyAsset(
+  algodClient: algosdk.Algodv2,
+  account: string,
+  assetId: number,
+  wallet: Wallet
+) {
+  const assetNumber = Number(assetId);
   // All of the created assets should now be back in the creators
   // Account so we can delete the asset.
   // If this is not the case the asset deletion will fail
@@ -164,10 +119,10 @@ export async function destroyAsset(algodClient: any, account: any, assetID: any,
   const confirmedTxn = await algosdk.waitForConfirmation(algodClient, txId, 10);
   console.log('Transaction ' + txId + ' confirmed in round ' + confirmedTxn['confirmed-round']);
   // The account3 and account1 should no longer contain the asset as it has been destroyed
-  console.log('Asset ID: ' + assetID);
+  console.log('Asset ID: ' + assetId);
   console.log('AccountAddr = ' + account);
-  await printCreatedAsset(algodClient, account, assetID);
-  await printAssetHolding(algodClient, account, assetID);
+  await printCreatedAsset(algodClient, account, assetId);
+  await printAssetHolding(algodClient, account, assetId);
 
   // return;
 }
