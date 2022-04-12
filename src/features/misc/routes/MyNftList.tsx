6c87cd39855bfd7d11bce6529668b56350aa19e7
pblanco-dekalabs <pblanco@dekalabs.com>
2022-04-12 12:47:47 +0200

    Added view flex.

--- src/features/misc/routes/MyNftList.tsx
+++ src/features/misc/routes/MyNftList.tsx
@@ -5,5 +5,20 @@ import { MainLayout } from '@/componentes/Layout/MainLayout';
  * minted user's NFTs, ongoing bids, sales...
  */
 export default function MyNftList() {
-  return <MainLayout>Participating.</MainLayout>;
+  return (
+    <MainLayout>
+      <div className="flex w-full">
+        <div className="columns-3">&nbsp;</div>
+        <div className="columns-1 flex bg-slate-200">
+          <div className="p-6 flex-1 shadow-lg rounded-lg bg-white">
+            ADO BADO BADO BASO DOAB BOSFB
+          </div>
+        </div>
+        <div className="columns-4">&nbsp;</div>
+        <div className="columns-10">
+          <h2>My NFTs</h2>
+        </div>
+      </div>
+    </MainLayout>
+  );
 }
