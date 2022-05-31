cde62d43c1ab2f9bc6df6d1b03fd5d2da3c87be7
pblanco-dekalabs <pblanco@dekalabs.com>
2022-05-31 11:06:39 +0200

    Declared direct listing provider stub.

--- src/service/impl/NullAVMProgramProvider.ts
+++ src/service/impl/NullAVMProgramProvider.ts
@@ -1,6 +1,10 @@
 import * as AVMProgramProvider from '@common/src/services/AVMProgramProvider';
+import * as AVMDirectListingProgramProvider from '@common/src/services/AVMDirectListingProgramProvider';
+
+console.warn('Registering AVMProgram Provider');
 
 @AVMProgramProvider.declare()
+@AVMDirectListingProgramProvider.declare()
 export default class NullAVMProgramProvider implements AVMProgramProvider.type {
   get auctionApprovalProgram(): Promise<Uint8Array> {
     throw new Error('Method not implemented.');
